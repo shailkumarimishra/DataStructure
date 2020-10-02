1ce339a0059ed320d5d68b131b487ad2d4706c48
shailkumarimishra <shailtinu2000@gmail.com>
2020-10-02 11:07:31 +0530

    stack implementation using array

---
+++ ds/src/stack/DynamicStack.java
@@ -0,0 +1,44 @@
+package stack;
+
+import java.util.ArrayList;
+
+public class DynamicStack {
+	private ArrayList<Integer> list;
+public DynamicStack() {
+	this.list=new ArrayList<>();
+}
+public void push(int data) {
+	list.add(data);
+}
+public int pop() {
+	return list.remove(list.size()-1);
+}
+public int size() {
+	return list.size();
+}
+public static void main(String[] args) {
+	DynamicStack stack=new DynamicStack();
+//	stack.pop();
+	stack.push(10);
+//	stack.pop();
+	stack.push(20);
+	stack.push(30);
+	stack.push(40);
+	stack.push(50);
+	stack.push(60);
+	stack.push(70);
+	stack.push(80);
+	stack.push(90);
+	stack.push(100);
+/*	System.out.println(stack.pop());//100
+	System.out.println(stack.pop());//90
+	System.out.println(stack.pop());//80
+	System.out.println(stack.size());//7*/
+	System.out.println("----------");
+	int count=stack.size();
+	for(int i=0;i<count;i++) {
+		System.out.println(stack.pop());
+	}
+	System.out.println(stack.pop());
+}
+}
---
+++ ds/src/stack/MyStack.java
@@ -0,0 +1,123 @@
+package stack;
+/*
+ * Application of stack:-
+ * 1. In function call
+ * 2. Checking for balanced expression
+ * 3. Reversing items
+ * 4. Infix to Prefix/Postfix
+ * 5. Undo/Redo or Forward/Backward
+ * 6. Stock span problems and it's variation******
+ * 
+ * Operations on Stack(Abstract data type)
+ * 1. push
+ * 2. pop
+ * 3. size
+ * 4. isEmpty
+ * 5. peek
+ * 
+ * Stack implementation using array(Fixed in size)
+ */
+public class MyStack {
+private int[] array;
+private int size;
+private static int capacity=10;
+public MyStack() {
+	this.array=new int[capacity];
+}
+/*
+ * 1.first check stack is full or not if it is full throw exception
+ * 2. else push the element and increase the size. 
+ * 
+ * T.C:- O(1)
+ */
+public void push(int data) {
+	if(size!=capacity)
+		array[size++]=data;
+	else 
+		throw new RuntimeException("stack overflow");
+}
+/*
+ * 1. first check stack is empty or not if empty then throw exception
+ * 2. else pop the element and decrease the size.
+ * 
+ * T.C:- O(1)
+ */
+public int pop() {
+	if(size>0) {
+	int result = this.array[size-1];
+	size--;
+	return result;
+	}
+	else {
+		throw new RuntimeException("stack underflow");
+	}
+}
+/*
+ * 1. first check stack is empty or not if empty then throw exception
+ * 2. else pop the element
+ * 
+ *  T.C:- O(1)
+ */
+public int peek() {
+	if(size>0) {
+		return this.array[size-1];
+	}
+	else {
+		throw new RuntimeException("stack underflow");
+	}
+}
+/*
+ * 1. check size==0 then true else false
+ * 
+ * T.C:-O(1)
+ */
+public boolean isEmpty() {
+	return size==0;
+}
+/*
+ * 1. return size
+ * 
+ * T.C:- O(1)
+ */
+public int size() {
+	return size;
+}
+public static void main(String[] args) {
+	MyStack stack=new MyStack();
+	System.out.println(stack.isEmpty());
+//	stack.pop();
+	stack.push(10);
+//	stack.pop();
+	stack.push(20);
+	stack.push(30);
+	stack.push(40);
+	stack.push(50);
+	System.out.println(stack.isEmpty());
+	stack.push(60);
+	stack.push(70);
+	stack.push(80);
+	stack.push(90);
+	stack.push(100);
+	System.out.println(stack.size());
+	System.out.println(stack.peek());
+/*	System.out.println(stack.pop());//100
+	System.out.println(stack.pop());//90
+	System.out.println(stack.pop());//80
+	System.out.println(stack.size());//7*/
+	System.out.println("----------");
+	int count=stack.size();
+	for(int i=0;i<count;i++) {
+		System.out.println(stack.pop());
+	}
+	System.out.println(stack.isEmpty());
+}
+}
+
+
+
+
+
+
+
+
+
