538616dfcab798c21bf0b853445bea5190f8f580
shailkumarimishra <shailtinu2000@gmail.com>
2020-11-10 08:47:51 +0530

    traversal

--- ds/src/tree/BinaryTree.java
+++ ds/src/tree/BinaryTree.java
@@ -26,83 +26,173 @@ import java.util.Queue;
  */
 public class BinaryTree {
 	private Node root;
-public void  insert(int data) {
-	Node newNode=new Node(null,data,null);
-	if(root==null) {
-		root=newNode;
-		return;
-	}
-	Queue<Node> queue=new LinkedList<>();
-	queue.add(root);
-	while(!queue.isEmpty()) {
-		Node currentNode = queue.remove();
-		if(currentNode.left==null) {
-			currentNode.left=newNode;
-			break;
-		}else if(currentNode.right==null){
-			currentNode.right=newNode;
-			break;
+	private int size;
+
+	public void insert(int data) {
+		Node newNode = new Node(null, data, null);
+		size++;
+		if (root == null) {
+			root = newNode;
+			return;
 		}
-		else {
-			queue.add(currentNode.left);
-			queue.add(currentNode.right);
+		Queue<Node> queue = new LinkedList<>();
+		queue.add(root);
+		while (!queue.isEmpty()) {
+			Node currentNode = queue.remove();
+			if (currentNode.left == null) {
+				currentNode.left = newNode;
+				break;
+			} else if (currentNode.right == null) {
+				currentNode.right = newNode;
+				break;
+			} else {
+				queue.add(currentNode.left);
+				queue.add(currentNode.right);
+			}
 		}
 	}
-}
-public void printInOrder() {
-	inOrderHelper(root);
-}
-private void inOrderHelper(Node root) {
-	if(root!=null) {
-		inOrderHelper(root.left);
-		System.out.print(root.data+" ");
-		inOrderHelper(root.right);
+
+	public void printLevelOrder() {
+		Queue<Node> queue = new LinkedList<>();
+		queue.add(root);
+		while (!queue.isEmpty()) {
+			Node currentNode = queue.remove();
+			System.out.print(currentNode.data + " ");
+			if (currentNode.left != null) {
+				queue.add(currentNode.left);
+			}
+			if (currentNode.right != null) {
+				queue.add(currentNode.right);
+			}
+		}
 	}
-}
-public void printPreOrder() {
-	preOrderHelper(root);
-}
-private void preOrderHelper(Node root) {
-	if(root!=null) {
-		System.out.print(root.data+" ");
-		preOrderHelper(root.left);
-		preOrderHelper(root.right);
+
+	public boolean contains(int data) {
+		return containsHelper(root, data);
 	}
-}
-public void printPostOrder() {
-	postOrderHelper(root);
-}
-private void postOrderHelper(Node root) {
-	if(root!=null) {
-		postOrderHelper(root.left);
-		postOrderHelper(root.right);
-		System.out.print(root.data+" ");
+	public boolean searchByLevelOrder(int data) {
+		Queue<Node> queue=new LinkedList<>();
+		queue.add(root);
+		while(!queue.isEmpty()) {
+			Node currentNode = queue.remove();
+			if(currentNode.data==data) {
+				return true;
+			}
+			if (currentNode.left != null) {
+				queue.add(currentNode.left);
+			}
+			if (currentNode.right != null) {
+				queue.add(currentNode.right);
+			}
+		}
+		return false;
 	}
-}
-private static class Node{
-	Node left;
-	int data;
-	Node right;
-	public Node(Node left, int data, Node right) {
-		this.left = left;
-		this.data = data;
-		this.right = right;
+
+	public boolean containsHelper(Node root, int data) {
+		if (root != null) {
+			containsHelper(root.left, data);
+			if (root.data == data) {
+				return true;
+			}
+			containsHelper(root.right, data);
+		}
+		return false;
 	}
-}
-public static void main(String[] args) {
-	BinaryTree tree=new BinaryTree();
-	tree.insert(10);
-	tree.insert(20);
-	tree.insert(30);
-	tree.insert(40);
-	tree.insert(50);
-	tree.insert(60);
-	tree.insert(70);
-	tree.insert(80);
-	tree.printInOrder();
-	System.out.println();
-	tree.printPreOrder();
-	System.out.println();
-	tree.printPostOrder();
-}
+
+	public boolean containsHelperPreOrder(Node root, int data) {
+		if (root != null) {
+			if (root.data == data) {
+				return true;
+			}
+			containsHelperPreOrder(root.left, data);
+			containsHelperPreOrder(root.right, data);
+		}
+		return false;
+	}
+
+	public void printInOrder() {
+		inOrderHelper(root);
+	}
+
+	private void inOrderHelper(Node root) {
+		if (root != null) {
+			inOrderHelper(root.left);
+			System.out.print(root.data + " ");
+			inOrderHelper(root.right);
+		}
+	}
+
+	public void printPreOrder() {
+		preOrderHelper(root);
+	}
+
+	private void preOrderHelper(Node root) {
+		if (root != null) {
+			System.out.print(root.data + " ");
+			preOrderHelper(root.left);
+			preOrderHelper(root.right);
+		}
+	}
+
+	public boolean containsHelperPostOrder(Node root, int data) {
+		if (root != null) {
+			containsHelperPostOrder(root.left, data);
+			containsHelperPostOrder(root.right, data);
+			if (root.data == data) {
+				return true;
+			}
+		}
+		return false;
+	}
+
+	public void printPostOrder() {
+		postOrderHelper(root);
+	}
+
+	private void postOrderHelper(Node root) {
+		if (root != null) {
+			postOrderHelper(root.left);
+			postOrderHelper(root.right);
+			System.out.print(root.data + " ");
+		}
+	}
+
+	public int size() {
+		return size;
+	}
+
+	private static class Node {
+		Node left;
+		int data;
+		Node right;
+
+		public Node(Node left, int data, Node right) {
+			this.left = left;
+			this.data = data;
+			this.right = right;
+		}
+	}
+
+	public static void main(String[] args) {
+		BinaryTree tree = new BinaryTree();
+		tree.insert(10);
+		tree.insert(20);
+		tree.insert(30);
+		tree.insert(40);
+		tree.insert(50);
+		tree.insert(60);
+		tree.insert(70);
+		tree.insert(80);
+		System.out.println(tree.size());
+		tree.printInOrder();
+		System.out.println();
+		tree.printPreOrder();
+		System.out.println();
+		tree.printPostOrder();
+		System.out.println();
+		System.out.println(tree.contains(100));
+		tree.printLevelOrder();
+		System.out.println();
+		System.out.println(tree.searchByLevelOrder(300));
+	}
 }
