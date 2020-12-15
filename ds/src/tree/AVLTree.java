4516b6032d5fea62146a995e4ab3b074c178f8fc
shailkumarimishra <shailtinu2000@gmail.com>
2020-12-15 08:54:42 +0530

    avltree

---
+++ ds/src/tree/AVLTree.java
@@ -0,0 +1,14 @@
+package tree;
+/*
+ * Operations on AVL Tree:-
+ * 1. insert
+ * 2. contains
+ * 3. traversal
+ * 4. delete
+ * 5. size
+ * 6. height
+ * for example:- 30,20,40,10,5,3,4,50,60,70,65
+ */
+public class AVLTree {
+
+}
