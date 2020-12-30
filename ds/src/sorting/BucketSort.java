c9eba1a58032c5e271a2cf9e0ef9da9383af3e1a
shailkumarimishra <shailtinu2000@gmail.com>
2020-12-30 08:41:31 +0530

    insertionsort

---
+++ ds/src/sorting/BucketSort.java
@@ -0,0 +1,11 @@
+package sorting;
+/*
+ * 1. Create no of Buckets=ceil/floor(sqt(total no of items))
+ * 2. Iterate through each no & place it in appropriate bucket
+ * 3. appropriate bucket=ceil(value*no of buckets)/max value in given array
+ * 4. sort all the buckets
+ * 5. merge all the buckets
+ */
+public class BucketSort {
+
+}
