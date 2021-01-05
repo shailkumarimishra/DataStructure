92109d2fc1aad1396e2700ed5fee747c9b7283de
shailkumarimishra <shailtinu2000@gmail.com>
2021-01-05 08:53:16 +0530

    sorting

---
+++ ds/src/sorting/QuickSort.java
@@ -0,0 +1,15 @@
+package sorting;
+/*
+ * 1. Quick Sort is a divide and conquer algorithm
+ * 2. At each step find pivot and then make sure all the smaller elements are on the
+ *    left of pivot & all Bigger elements are on the right of pivot.
+ * 3. It does this recursively until the entire array is sorted.
+ * 4. Unlike merge sort it does not require any extra space.
+ * e.g; 9,4,6,3,7,1,2,11,5
+ * Note:- Assume p is pivot element & j is first element(j=0) and i=-1;
+ * compare j<=p then swap i & j.
+ * 
+ */
+public class QuickSort {
+
+}
