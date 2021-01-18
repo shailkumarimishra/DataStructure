199249489c261a0f5b066660795d1e9ef556ac75
shailkumarimishra <shailtinu2000@gmail.com>
2021-01-18 08:36:43 +0530

    graph

---
+++ ds/src/graph/GraphDemo.java
@@ -0,0 +1,30 @@
+package graph;
+/*
+ * Graph:- Graph is a pair of sets (V,E) where V is the set of vertices and E is the set
+ * 		   of edges connecting the pair of vertices. 
+ * 
+ * Terminology of Graph:-
+ * 1. Vertices-> Vertices are the nodes of the graph.
+ * 2. Edges-> Edges are arc that connect pair of Vertices.
+ * 3. UnWeighted Graph-> A Graph not having a weight associated with any edge.
+ * 4. Weighted Graph-> A Graph having weight associated with edge.
+ * 5. Directed Graph-> It is a Graph that is a set of Vertices connected by edges where
+ * 					   the edges have a direction associated with them.
+ * 6.UnDirected Graph->It is a Graph that is a set of Vertices connected by edges where
+ * 					   the edges do not have direction associated with them.
+ * 7.Acyclic Graph-> A Graph having no loop.
+ * 8.Cyclic Graph-> A Graph having loop.
+ * 
+ * Types Of Graph:-
+ * 1. Directed Graph and Undirected Graph
+ * 2. Directed Weighted and Directed UnWeighted Graph.
+ * 3. UnDirected Weighted and UnDirected UnWeighted Graph.
+ * 4. Directed Positive Weighted Graph and Directed Negative Weighted Graph.
+ * 5. UnDirected Positive Weighted Graph and UnDirected Negative Weighted Graph.
+ * 
+ * Note:- A tree is a special type of Graph (Acyclic,UnDirected) but vice versa is not
+ * 		  true.
+ */
+public class GraphDemo {
+
+}
